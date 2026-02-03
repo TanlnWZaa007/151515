20720fb796f226f5130c227f9cbe98d25965285e
TanlnWZaa007 <153242404+TanlnWZaa007@users.noreply.github.com>
2026-02-03 14:37:12 +0700

    5

--- lab15_5.cpp
+++ lab15_5.cpp
@@ -20,3 +20,20 @@ int main(){
 
 //Do not modify the code above this line
 //Write definition of shuffle() using pointer after this line. 
+
+
+void shuffle(int *a, int *b, int *c, int *d){
+	int temp[] = {*a, *b, *c, *d};
+	
+	for(int i = 0; i < 4; i++){
+		int x = rand() % 4; 
+		int t = temp[i];
+		temp[i] = temp[x];
+		temp[x] = t;
+	}
+	
+	*a = temp[0];
+	*b = temp[1];
+	*c = temp[2];
+	*d = temp[3];
+}
